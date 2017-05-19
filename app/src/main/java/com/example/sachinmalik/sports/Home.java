f98b777153319bcba5bf42a5f83160df0d3195e2
Sachin Malik <sachin.15085@ggnindia.dronacharya.info>
2017-05-19 16:22:08 +0530

    update done

--- app/src/main/java/com/example/sachinmalik/sports/Home.java
+++ app/src/main/java/com/example/sachinmalik/sports/Home.java
@@ -1,13 +1,32 @@
 package com.example.sachinmalik.sports;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.sachinmalik.sports.fragments.ItemFragment;
+
+import mehdi.sakout.fancybuttons.FancyButton;
 
 public class Home extends AppCompatActivity {
+    FancyButton home;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        home=(FancyButton) findViewById(R.id.home_button);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                home.setVisibility(View.GONE);
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.add(R.id.your_placehodler, new ItemFragment());
+                ft.commit();
+            }
+        });
     }
 }
